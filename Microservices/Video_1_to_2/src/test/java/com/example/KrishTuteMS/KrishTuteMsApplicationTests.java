ccada68be07d9d472f96684c08fb17a889f53112
Bhawantha <geeganagebawantha@gmail.com>
2022-04-15 15:27:07 +0530

    upload MS

---
+++ Microservices/Video_1_to_2/src/test/java/com/example/KrishTuteMS/KrishTuteMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KrishTuteMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrishTuteMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Microservices/Video_6_to_7/rentCarProjectKrish/src/test/java/com/bawantha/rentCarProjectKrish/Video5To6Tests.java
@@ -0,0 +1,13 @@
+package com.bawantha.rentCarProjectKrish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Video5To6Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
